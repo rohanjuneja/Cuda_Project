23b7fb23e7b710562237dfb801c80738bbf1d9c7
Rohan Juneja <junejarohan25@gmail.com>
2017-06-30 20:53:21 +0530

    first commit

---
+++ v1.2.1.1/PotField/potVect_dynamic.cu
@@ -0,0 +1,697 @@
+// ----
+// ----  Computes the potential field for a volume
+// ----  Input: volume file, dimensions: X, Y, Z, output file name
+// ----	 Output: normalized potential field:
+//		 1 vector for each point in the volume
+//
+// Last change: Thu May 15 15:20:38 EDT 2003 by Nicu D. Cornea
+//
+//
+
+// #define TRACE
+
+
+#include "potVect.h"
+#include <thrust/sort.h>
+#include <thrust/device_vector.h>
+#include <thrust/functional.h>
+#include <thrust/device_ptr.h>
+#include <thrust/reduce.h>
+
+#define BOUND_SIZE	7000
+struct compareStruct {
+	__host__ __device__
+	bool operator()(VoxelPosition a, VoxelPosition b) {
+		if(a.z != b.z)
+			return a.z < b.z;
+		else if(a.y != b.y)
+			return a.y < b.y;
+		else
+			return a.x < b.x;
+	}
+};
+struct reduceStruct {
+	 __device__ Vector operator()(Vector a, Vector b) {
+		Vector temp;
+		temp.xd=a.xd+b.xd;
+		temp.yd=a.yd+b.yd;
+		temp.zd=a.zd+b.zd;
+		return temp;
+	}
+};
+
+__constant__ VoxelPosition d_bound[BOUND_SIZE];
+
+bool SortBoundaryArray(int numBound, VoxelPosition Bound[]);
+
+
+/*
+this kernel normalizes the force field by calculating the multiplication of magnitudes of force in
+x,y,z direction and dividing it by the final multiplication.
+*/
+__global__ void normalize_vector(Vector* force,unsigned char* f, bool inOut,int slsz,int L)
+{
+	int k=blockIdx.x;
+	int j=threadIdx.x;
+	int i=blockIdx.y;
+	int idx=k*slsz + j*L + i;
+	if(!inOut) {
+    // only for interior voxels we had calculated forces
+    if(f[idx] == EXTERIOR) return;
+  }
+  
+  float r = force[idx].xd*force[idx].xd + 
+      force[idx].yd*force[idx].yd + 
+      force[idx].zd*force[idx].zd;
+    
+  if(r > 0.00) {
+    r = sqrtf(r);
+    
+    force[idx].xd = force[idx].xd / r;
+    force[idx].yd = force[idx].yd / r;
+    force[idx].zd = force[idx].zd / r;
+  }
+	
+
+}
+/*
+This kernel computes potential field at every inside voxel using 
+boundary voxel as point charges
+*/
+__global__ void dynamic_kernel(int i,int j,int k,int fieldStrenght,Vector* result,int size)
+{
+	
+	int s=blockIdx.x*blockDim.x+threadIdx.x;
+	if(s<size)
+	{
+				float v1 = i - d_bound[s].x;
+				float v2 = j - d_bound[s].y;
+				float v3 = k - d_bound[s].z;
+				float r, t;
+				#ifdef EUCLIDEAN_METRIC
+				// euclidean metric
+					r = sqrtf(v1*v1 + v2*v2 + v3*v3);
+				#else
+				// simpler metric
+					r = abs(v1) + abs(v2) + abs(v3);
+				#endif
+
+			
+				if(r != 0.00) {
+				
+				  
+				  t = 1.00;
+				  for(int p = 0; p <= fieldStrenght; p++) {
+				    t = t * r;
+				  }
+				  r = t;
+				}
+			
+	
+			result[s].xd=(v1/r);
+			result[s].yd=(v2/r);
+			result[s].zd=(v3/r);
+			if(s==6554)
+			printf("child kernel:%d %d %.6f %.6f %.6f %.6f %.6f %.6f\n",s,size,d_bound[4].x,d_bound[4].y,d_bound[4].z,v1,v2,v3);
+
+	}
+
+
+
+
+}
+
+__global__ void compute_potential_field(Vector* force,int numBound,unsigned char* f,bool inOut,int slsz,int sz,int L, int fieldStrenght)
+{
+	int k=blockIdx.x;
+	int j=threadIdx.x;
+	int i=blockIdx.y;
+
+	int zStartIndex = 0;
+	int zEndIndex = numBound- 1;
+	if((k*slsz + j*L + i)==0)
+	printf("parent kernel %.6f %.6f %.6f\n",d_bound[4].x,d_bound[4].y,d_bound[4].z );
+	int s;
+	for (s = 0; s < numBound; s++) {
+		if((k - d_bound[s].z) <= PF_THRESHOLD) {
+			zStartIndex = s;
+			break;
+		}
+	}
+	for (s = numBound-1; s >= zStartIndex; s--) {
+		if((d_bound[s].z - k) <= PF_THRESHOLD) {
+			zEndIndex = s;
+			break;
+		}
+	}
+
+	int yStartIndex = zStartIndex;
+	int yEndIndex = zEndIndex;
+		for (s = zStartIndex; s <= zEndIndex; s++) {
+			if((j - d_bound[s].y) <= PF_THRESHOLD) {
+				yStartIndex = s;
+				break;
+			}
+		}
+		for (s = zEndIndex; s >= yStartIndex; s--) {
+			if((d_bound[s].y - j) <= PF_THRESHOLD) {
+				yEndIndex = s;
+				break;
+			}
+		}
+
+	int idx=k*slsz + j*L + i;
+	force[idx].xd = 0.00;
+	force[idx].yd = 0.00;
+	force[idx].zd = 0.00;
+
+	
+	if(!inOut) {
+			  if(f[idx] == 0) {
+			    return;
+			  }
+			}
+	
+
+	if(f[idx] == SURF) return;
+	if(f[idx] == BOUNDARY) return;
+
+	int startIndex = yStartIndex;
+	int endIndex = yEndIndex;
+	for (s = yStartIndex; s <= yEndIndex; s++) {
+		if((i - d_bound[s].x) <= PF_THRESHOLD) {
+			startIndex = s;
+			break;
+				}
+			}
+	for (s = yEndIndex; s >= startIndex; s--) {
+		if((d_bound[s].x - i) <= PF_THRESHOLD) {
+			endIndex = s;
+			break;
+		}
+	}
+
+	if(endIndex < startIndex) {
+				
+				startIndex = 0;
+				endIndex = numBound - 1;
+	}
+	
+	int size=endIndex- startIndex+1;
+	dim3 dimBlock(1024,1);
+	
+	int block=size/1024;
+	if(1024*block<size)
+		block+=1;
+	dim3 dimGrid(block,1);
+	//result=(Vector *)malloc(sizeof(Vector)*size);
+	Vector *result;
+	cudaMalloc((void **)&result,sizeof(Vector)*size);
+	
+	// cudaStream_t stream;
+	// cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
+	// dynamic_kernel<<<dimGrid,dimBlock>>>(i,j,k,fieldStrenght,result,size);
+
+	//for(int i=0; i<size; i++) {
+	//	printf("%.6f %.6f %.6f\n", result[i].xd, result[i].yd, result[i].zd);
+	//}
+
+	// cudaError_t err;
+	// cudaStreamSynchronize(s);
+	// cudaStreamDestroy(stream);
+	// printf("Rohan before cuda device sync\n");
+
+
+	// cudaDeviceSynchronize();
+
+
+
+	// if ((err = cudaDeviceSynchronize()) != cudaSuccess){
+	// 	printf("There some error\n");
+	// }
+	// printf("Rohan after cuda device sync\n");
+	// printf("Rohan:%d %.6f %.6f %.6f\n",size,result[0].xd,result[0].yd,result[0].zd);
+		for(int j=0;j<size;j++)
+			printf("%.6f %.6f %.6f\n",result[j].xd,result[j].yd,result[j].zd);
+	// thrust::device_ptr<Vector> cptr = thrust::device_pointer_cast(result);
+	//  // thrust::device_ptr<cufftReal> dev_ptr(out1_d); 
+ //        thrust::device_vector<Vector> val_vec(size); 
+ //        thrust::copy(thrust::seq,cptr, cptr + size, val_vec.begin()); 
+	// // thrust::device_vector<Vector> cptr(result,result+size);
+	reduceStruct func;
+	Vector init;
+	init.xd=1;
+	init.yd=1;
+	init.zd=1;
+	int zun;
+	
+	// for(int i=0; i<size; i++) {
+	// 	init.xd += result[i].xd;
+	// 	init.yd += result[i].yd;
+	// 	init.zd += result[i].zd;
+	// }
+
+	Vector final;
+	// Vector final= thrust::reduce(thrust::seq,result, result+size,init,func);
+	// if(final.xd!=0)
+		// printf("%.6f %.6f %.6f\n",final.xd,final.yd,final.zd);
+	
+	final = init;
+	if(final.xd!=0)
+		printf("%.6f %.6f %.6f\n",init.xd,init.yd,init.zd);
+	force[idx].xd=init.xd;
+	force[idx].yd=init.yd;
+	force[idx].zd=init.zd;
+
+// 	for (s = startIndex; s <= endIndex; s++) {
+				
+// 				float v1 = i - Bound[s].x;
+// 				float v2 = j - Bound[s].y;
+// 				float v3 = k - Bound[s].z;
+// 				float r, t;
+// #ifdef EUCLIDEAN_METRIC
+// 				// euclidean metric
+// 				r = sqrtf(v1*v1 + v2*v2 + v3*v3);
+// #else
+// 				// simpler metric
+// 				r = abs(v1) + abs(v2) + abs(v3);
+// #endif
+
+			
+// 				if(r != 0.00) {
+				
+				  
+// 				  t = 1.00;
+// 				  for(int p = 0; p <= fieldStrenght; p++) {
+// 				    t = t * r;
+// 				  }
+// 				  r = t;
+				  
+
+// 				  force[idx].xd+=(v1/r);
+// 				  force[idx].yd+=(v2/r);
+// 				  force[idx].zd+=(v3/r);
+// 				}
+// 			}
+			
+}
+
+/*
+This kernel computes potential field at every boundary voxel using 
+the neighbours stored in ng-array(shared memory)
+*/
+__global__ void computePotentialFieldForBoundaryVoxels(unsigned char* f, Vector* force, int slsz, bool inOut, int L) {
+	
+	int k=blockIdx.x+1;
+	int j=threadIdx.x+1;
+	int i=blockIdx.y+1;
+	__shared__ int ng[26];
+	if(threadIdx.x==0)
+	{
+		 // face neighbors
+			    ng[0]	= + slsz + 0 + 0;
+			    ng[1]	= - slsz + 0 + 0;
+			    ng[2]	= +    0 + L + 0;
+			    ng[3]	= +    0 - L + 0;
+			    ng[4]	= +    0 + 0 + 1;
+			    ng[5]	= +    0 + 0 - 1;
+			    // v-neighbors
+			    ng[6]	= - slsz - L - 1;
+			    ng[7]	= - slsz - L + 1;
+			    ng[8]	= - slsz + L - 1;
+			    ng[9]	= - slsz + L + 1;
+			    ng[10]	= + slsz - L - 1;
+			    ng[11]	= + slsz - L + 1;
+			    ng[12]	= + slsz + L - 1;
+			    ng[13]	= + slsz + L + 1;
+			    // e-neighbors
+			    ng[14]	= + slsz + L + 0;
+			    ng[15]	= + slsz - L + 0;
+			    ng[16]	= - slsz + L + 0;
+			    ng[17]	= - slsz - L + 0;
+			    ng[18]	= + slsz + 0 + 1;
+			    ng[19]	= + slsz + 0 - 1;
+			    ng[20]	= - slsz + 0 + 1;
+			    ng[21]	= - slsz + 0 - 1;
+			    ng[22]	= +    0 + L + 1;
+			    ng[23]	= +    0 + L - 1;
+			    ng[24]	= +    0 - L + 1;
+			    ng[25]	= +    0 - L - 1;
+	}
+	__syncthreads();	
+	long idx = k*slsz + j*L + i;
+	  
+	if((f[idx] == SURF) ||
+	 (f[idx] == BOUNDARY))
+	{
+	  force[idx].xd = 0.00;
+	  force[idx].yd = 0.00;
+	  force[idx].zd = 0.00;
+	  float var_xd=0.00;
+	  float var_yd=0.00;
+	  float var_zd=0.00;
+	  // look at the neighbors and average the forces if not 0
+	  //
+	int v1 = 0;
+	for(int s=0; s < 26; s++) {
+
+	long iidx = idx + ng[s];		
+	if(f[iidx] == SURF)		continue;
+	if(f[iidx] == BOUNDARY)	continue;
+
+	// if we know the interior of the object, take only interior
+	// neighbors
+	if(!inOut) {
+	  if(f[iidx] == EXTERIOR)	continue;
+	}
+
+	var_xd = var_xd + force[iidx].xd;
+	var_yd = var_yd + force[iidx].yd;
+	var_zd = var_zd + force[iidx].zd;
+	v1 = v1 + 1;
+
+	  }
+	  
+	  // average
+	  if(v1 != 0) {
+	var_xd = var_xd / (double) v1;
+	var_yd= var_yd / (double) v1;
+	var_zd = var_zd / (double) v1;
+	  }
+	  else {
+	printf("Boundary voxel has no interior neighbor !!! - Force = 0\n");
+	  }
+	  
+	  // normalize
+	float r = var_xd*var_xd + 
+	var_yd*var_yd + 
+	var_zd*var_zd;
+	  
+	  if(r > 0.00) {
+	r = sqrtf(r);
+
+	force[idx].xd = var_xd / r;
+	force[idx].yd = var_yd / r;
+	force[idx].zd = var_zd/ r;	
+	  }
+	}
+}
+
+
+
+bool CalculatePotentialField(
+	int L, int M, int N, 	      // [in] size of volume
+	unsigned char* f, 	      // [in] volume flags
+	int fieldStrenght,	      // [in] potential field strenght
+	Vector* force,		      // [out] force field	
+	bool inOut                    // [in] flag indicating that we don't 
+	                              //    know what the inside/outside of 
+	                              //    the object is. We have only point 
+	                              //    samples of the boundary.
+	                              //  DEFAULT: false (only interior)
+) {
+	//cudaSetDevice(1);
+  int Lm1, Mm1, Nm1;
+  int i,j,k, s, p;
+  long idx, iidx, slsz, sz;
+  VoxelPosition* Bound;
+  int numBound = 0;
+  bool flagSurf, flagBound;
+  double r, t;
+  int v1, v2, v3;
+  int startIndex, tmpStartIndex, endIndex, tmpEndIndex, zStartIndex, zEndIndex, yStartIndex, yEndIndex;
+
+  //
+  // check volume padding - fast version
+  //
+  if(!CheckVolumePadding(f, L, M, N)) {
+    printf("** Error - Object touches bounding box. Abort.\n");
+    exit(1);
+  }
+
+
+#ifdef _DEBUG
+	printf("\t************ Potential Field calculation parameters: ******************\n");
+#ifdef HALF_BOUNDARY_POINTS
+	printf("\t** Using only HALF of the boundary points.\n");
+#else
+	printf("\t** Using ALL boundary points.\n");
+#endif
+
+#ifdef EUCLIDEAN_METRIC
+	printf("\t** Using EUCLIDEAN metric.\n");
+#else
+	printf("\t** Using NON EUCLIDEAN metric.\n");
+#endif	
+	if(inOut) {
+	  printf("\t** Inside and Outside.\n");
+	}
+	else {
+	  printf("\t** Inside ONLY.\n");
+	}
+	printf("\t********* Potential Field calculation parameters - end ****************\n");
+
+#endif
+
+  if((Bound = new VoxelPosition[BOUND_SIZE]) == NULL) {
+	printf("\nERROR allocating memory for boundary array! - Abort\n");
+	exit(1);
+  }
+
+  Lm1 = L - 1;
+  Mm1 = M - 1;
+  Nm1 = N - 1;
+  slsz = L*M;		// slice size
+  sz = slsz*N;
+
+  // save all the boundary voxels in array Bound[]
+	for (k = 1; k < Nm1; k++) {
+		for (j = 1; j < Mm1; j++) {
+			for (i = 1; i < Lm1; i++) {
+				flagSurf = false;
+				flagBound = true;
+				idx = k*slsz + j*L + i;
+
+				// CASE 1: treat the inner layer
+				if (f[idx] == 0) continue;
+
+				//consider six face neighbors, if anyone is zero, it is a boundary voxel
+				iidx = k*slsz + j*L + i-1;
+				if (f[iidx] == 0) {
+					flagSurf = true;
+				}
+#ifdef HALF_BOUNDARY_POINTS
+				// consider only half of the boundary points
+				else {
+					if (f[iidx] == BOUNDARY) {
+						// a neighbour of the point was already selected so we will not select this one as part of the boundary.
+						flagBound = false;
+					}
+
+				}
+#endif
+
+				if(!flagSurf || flagBound) {
+
+				iidx = k*slsz + j*L + i+1;
+				if (f[iidx] == 0) {
+					flagSurf = true;
+				}
+#ifdef HALF_BOUNDARY_POINTS
+				// consider only half of the boundary points
+				else {
+					if (f[iidx] == BOUNDARY) {
+						// a neighbour of the point was already selected so we will not select it as part of the boundary.
+						flagBound = false;
+					}
+				}
+#endif
+
+				if(!flagSurf || flagBound) {
+
+				iidx = k*slsz + (j-1)*L + i;
+				if (f[iidx] == 0) {
+					flagSurf = true;
+				}
+#ifdef HALF_BOUNDARY_POINTS
+				// consider only half of the boundary points
+				else {
+					if (f[iidx] == BOUNDARY) {
+						// a neighbour of the point was already selected so we will not select it as part of the boundary.
+						flagBound = false;
+					}
+				}
+#endif
+
+				if(!flagSurf || flagBound) {
+
+				iidx = k*slsz + (j+1)*L + i;
+				if (f[iidx] == 0) {
+					flagSurf = true;
+				}
+#ifdef HALF_BOUNDARY_POINTS
+				// consider only half of the boundary points
+				else {
+					if (f[iidx] == BOUNDARY) {
+						// a neighbour of the point was already selected so we will not select it as part of the boundary.
+						flagBound = false;
+					}
+				}
+#endif
+
+				if(!flagSurf || flagBound) {
+
+				iidx = (k-1)*slsz + j*L + i;
+				if (f[iidx] == 0) {
+					flagSurf = true;
+				}
+#ifdef HALF_BOUNDARY_POINTS
+				// consider only half of the boundary points
+				else {
+					if (f[iidx] == BOUNDARY) {
+						// a neighbour of the point was already selected so we will not select it as part of the boundary.
+						flagBound = false;
+					}
+				}
+#endif
+
+				if(!flagSurf || flagBound) {
+
+				iidx = (k+1)*slsz + j*L + i;
+				if (f[iidx] == 0) {
+					flagSurf = true;
+				}
+#ifdef HALF_BOUNDARY_POINTS
+				// consider only half of the boundary points
+				else {
+					if (f[iidx] == BOUNDARY) {
+						// a neighbour of the point was already selected so we will not select it as part of the boundary.
+						flagBound = false;
+					}
+				}
+#endif
+
+				}
+				}
+				}
+				}
+				}
+
+				// restore idx to the right value
+				idx = k*slsz + j*L + i;
+				if (flagSurf) {
+					f[idx] = SURF;
+
+					if(flagBound) {
+							// if no neighbour of this voxel is already marked as boundary, then mark this one.
+							// or if we are taking all the boundary voxels 
+							// 	(in this case flagBound stays true)
+						f[idx] = BOUNDARY;
+						Bound[numBound].x = i;
+						Bound[numBound].y = j;
+						Bound[numBound].z = k;
+						numBound++;
+						if(numBound >= BOUND_SIZE) {
+							printf("ERROR: too many boundary points detected !! - Abort.\n");
+							exit(1);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	//printf("numBound = %d \n", numBound);
+
+#ifdef _DEBUG
+	PrintElapsedTime("\tPF-1: finding the boundary voxels.");
+	printf("\t--Found %d boundary voxels.\n", numBound);
+#endif
+
+
+// sort the boundary array.
+SortBoundaryArray(numBound, Bound);
+
+#ifdef _DEBUG
+	PrintElapsedTime("\tPF-2: sorting the boundary voxels.");
+#ifdef TRACE
+	// print the boundary voxels
+	for(i=0; i < numBound; i++) {
+		printf("%d %d %d 0.5\n", Bound[i].x, Bound[i].y, Bound[i].z);
+	}
+	exit(1);
+#endif	
+
+#endif
+
+
+// Compute the potential field
+	printf("Computing potential field.\n");
+	dim3 dimBlock(M,1);
+	dim3 dimGrid(N,L);
+	// VoxelPosition *d_bound;
+	unsigned char* d_f;
+	Vector* d_force;
+	cudaMalloc((void **)&d_f,sizeof(unsigned char)*L*M*N);
+	// cudaMalloc((void **)&d_bound,sizeof(VoxelPosition)*BOUND_SIZE);
+	cudaMalloc((void **)&d_force,sizeof(Vector)*L*M*N);
+	cudaMemcpy(d_f,f,sizeof(unsigned char)*L*M*N,cudaMemcpyHostToDevice);
+	// cudaMemcpy(d_bound,Bound,sizeof(VoxelPosition)*BOUND_SIZE,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol(d_bound,Bound, numBound* sizeof(VoxelPosition));
+	cudaMemcpy(d_force,force,sizeof(Vector)*L*M*N,cudaMemcpyHostToDevice);
+	compute_potential_field<<<dimGrid,dimBlock>>>(d_force,numBound,d_f,inOut,slsz,sz,L, fieldStrenght);
+	
+	normalize_vector<<<dimGrid,dimBlock>>>(d_force,d_f,inOut,slsz,L);
+
+
+delete [] Bound;
+
+
+#ifdef _DEBUG
+	PrintElapsedTime("\tPF-3: computing potential field for inside voxels.");
+#endif
+
+#ifdef _DEBUG
+  PrintElapsedTime("\tPF-4: normalizing force vectors for inside voxels.");
+#endif
+
+  if (!inOut) {
+    //neighbors:
+    
+
+    dim3 dimBlock(Mm1-1,1);
+	dim3 dimGrid(Nm1-1,Lm1-1);
+    
+
+	computePotentialFieldForBoundaryVoxels<<<dimGrid,dimBlock>>>(d_f, d_force, slsz, inOut, L);
+
+
+    
+  }
+  cudaMemcpy(force,d_force,sizeof(Vector)*L*M*N,cudaMemcpyDeviceToHost);
+  
+
+#ifdef _DEBUG
+	PrintElapsedTime("\tPF-5: computing potential field for boundary voxels.");
+#endif
+
+
+  return true;
+}
+
+
+
+
+compareStruct comp;
+
+bool SortBoundaryArray(int numBound, VoxelPosition Bound[]) {
+	thrust::sort(Bound, Bound+numBound, comp);
+	return true;
+}
+
+
+
+
+
+
+
